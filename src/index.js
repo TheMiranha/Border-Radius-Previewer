79d5340f9cc088c98e30fbab4924ccc6d2408874
TheMiranha <53923502+TheMiranha@users.noreply.github.com>
2021-03-04 17:18:46 -0300

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    constructor(props) {
+        super(props);
+        this.state = {
+            borderBottomLeftRadius: 20,
+            borderBottomRightRadius: 20,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+        }
+    }
+
+    getCss = () => {
+        var {borderTopLeftRadius,borderTopRightRadius, borderBottomRightRadius, borderBottomLeftRadius } = this.state;
+        return 'border-radius: '+borderTopLeftRadius+'px '+borderTopRightRadius+'px '+borderBottomRightRadius+'px '+borderBottomLeftRadius+'px';
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="container">
+                    <div className="title">Border-radius Previewer</div>
+                    <div className="box" style={this.state}></div>
+                    <div className="variáveis">
+                        T: Topo; B: Baixo; D: Direta; E: Esquerda
+                    </div>
+                    <div className="inputs">
+                        <div>TD: <input className="slot" placeholder="TD" type="number"
+                        value={this.state.borderTopRightRadius} onChange={a => { this.setState({borderTopRightRadius: parseInt(a.target.value)});console.log(this.state.borderTopRightRadius)}}/></div>
+                        <div>
+                        TE: <input className="slot" placeholder="TE" type="number"
+                        value={this.state.borderTopLeftRadius} onChange={a => this.setState({borderTopLeftRadius: parseInt(a.target.value)})}/>
+                        </div>
+                       <div>
+                       BD: <input className="slot" placeholder="BD" type="number"
+                        value={this.state.borderBottomRightRadius} onChange={a => this.setState({borderBottomRightRadius: parseInt(a.target.value)})}/>
+                       </div>
+                        <div>BE: <input className="slotp" placeholder="BE" type="number"
+                        value={this.state.borderBottomLeftRadius} onChange={a => this.setState({borderBottomLeftRadius: parseInt(a.target.value)})}/>
+                        </div>
+                    </div>
+                    <div className="result">
+                        <input value={this.getCss()}/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
